873d2855fec1fa40d293e1532cdc7e698aefd7c3
Haley Someone <hbarlar1@gmail.com>
2018-07-29 21:01:26 +0000

    automatically backed up by learn

--- spec/tweet_shortener_spec.rb
+++ spec/tweet_shortener_spec.rb
@@ -23,6 +23,26 @@ describe 'tweet shortener' do
     end
   end
 
+def dictionary
+  dictionary = {
+    "hello" => "hi",
+    "to, two, too" => "2",
+    "for, four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+  
+  
+def word_substituter(tweet)
+  tweet.split(" ").collect |x|
+  
+end
+  
+
   # Question 2
   describe '#bulk_tweet_shortener' do  
     it 'shortens each tweet and prints the results' do
