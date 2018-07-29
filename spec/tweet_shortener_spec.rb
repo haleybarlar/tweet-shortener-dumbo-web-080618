3abc2445a36c92c0ba0e26b1faa7cf7b87acac6d
Haley Someone <hbarlar1@gmail.com>
2018-07-29 21:10:55 +0000

    automatically backed up by learn

--- spec/tweet_shortener_spec.rb
+++ spec/tweet_shortener_spec.rb
@@ -36,7 +36,7 @@ def dictionary
 end
   
 def word_substituter(tweet)
-  tweet.split(" ").collect |word|
+  tweet.split(" ").collect do |word|
   if dictionary.keys.include?(word.downcase)
     word = dictionary[word.downcase]
   else
