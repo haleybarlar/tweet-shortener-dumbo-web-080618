0c9d74824d92259c421c957e5d6afc3da411f36d
Haley Someone <hbarlar1@gmail.com>
2018-07-29 21:28:58 +0000

    automatically backed up by learn

--- spec/tweet_shortener_spec.rb
+++ spec/tweet_shortener_spec.rb
@@ -26,8 +26,11 @@ describe 'tweet shortener' do
 def dictionary
   words = {
     "hello" => "hi",
-    "to, two, too" => "2",
-    "for, four" => "4",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
     "be" => "b",
     "you" => "u",
     "at" => "@",
